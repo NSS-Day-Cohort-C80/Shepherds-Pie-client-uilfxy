681f70338451d1c8b5676e67a3db085bb222823e
Eric Martinez <ericmartinez1007@gmail.com>
2026-04-07 10:55:35 -0500

    Added routes to index.jsx, App.jsx, and ApplicationViews.jsx, and added an Authorized protection jsx

---
+++ views/ApplicationViews.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+export const ApplicationViews = () => {
+    const [currentUser, setCurrentUser] = useState({})
+
+    useEffect(() => {
+        const localShepherdUser = localStorage.getItem("shepherd_user")
+        const shepherdUserObj = JSON.parse(localShepherdUser)
+
+        setCurrentUser(shepherdUserObj)
+    }, [])
+
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <>
+                        <NavBar currentUser={currentUser} />
+                        <Outlet />
+                    </>
+                }
+            >
+                <Route index element={<Welcome />} />
+                <Route path="order" element={<Order currentUser={currentUser} />} />
+                <Route path="review" element={<Review currentUser={currentUser} />} />
+            </Route>
+        </Routes>
+    )
+}
